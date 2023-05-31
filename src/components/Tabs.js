dce96fc5c716229a259425f20d322bf71ff7d224
Kapil Chaudhary <kc685838@gmail.com>
2023-05-31 15:35:09 +0530

    submitting code-Wed May 31 2023 15:35:07 GMT+0530 (India Standard Time)

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 
 const Tabs = (props) => {
-    const [content, setContent] = useState("");
-
     const {arr} = props;
+    const [content, setContent] = useState(arr[0].content);
+
+    
     return (
         <div>
             <ul>
-                {arr.map((item) => {
-                    return <li onClick={() => {setContent(item.content)}}>{item.title}</li>;
+                {arr.map((item, index) => {
+                    return <li key={index} onClick={() => {setContent(item.content)}}>{item.title}</li>;
                 })}
             </ul>
             <p>{content}</p>
